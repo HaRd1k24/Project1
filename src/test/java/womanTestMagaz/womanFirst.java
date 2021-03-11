eecec1064e8bae652a119dc9fe3afb8d7faa5df4
HaRd1k24 <leva.trapeznikov@mail.ru>
2021-03-11 17:08:17 +0300

    Initial commit

---
+++ src/test/java/womanTestMagaz/womanFirst.java
@@ -0,0 +1,43 @@
+package womanTestMagaz;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pages.BuyProduct;
+import pages.MainPage;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class womanFirst  {
+
+    private final MainPage mainPage = new MainPage();
+    private final BuyProduct buyProduct = new BuyProduct();
+
+
+    @BeforeEach
+    public void setup(){
+        Configuration.browser = "Chrome";
+        Configuration.startMaximized = true;
+        open("http://automationpractice.com/");
+
+    }
+    @Test
+    public void checkWomanPage(){
+        final String women  = "WOMEN";
+        mainPage.clickButton();
+        mainPage.search(women);
+
+    }
+    @Test
+    public void searchProduct() {
+        String clothes = "Blouse";
+        Condition a = visible;
+        Condition b = exist;
+        buyProduct.findClothes(clothes);
+        buyProduct.findBlouse(a);
+        buyProduct.searchResult(b);
+    }
+    }
+
